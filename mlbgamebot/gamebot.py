706969bb634b680da63a834140763b9d2f2a9047
Ido Avraham <40628873+idoavrah@users.noreply.github.com>
2023-07-10 08:22:15 +0000

    test

--- mlbgamebot/gamebot.py
+++ mlbgamebot/gamebot.py
@@ -5,8 +5,6 @@ import time
 
 if __name__ == "__main__":
 
-    while True:
-        sync.start()
-        asyncio.run(parse.start())
-        asyncio.run(parse.daily())
-        time.sleep(60)
+    sync.start()
+    asyncio.run(parse.start())
+    asyncio.run(parse.daily())
